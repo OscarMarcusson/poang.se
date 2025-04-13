fe83f0f6d4e91566cd62e053a23af8eaaf9e20c2
OscarMarcusson <oscar.marcusson@outlook.com>
2025-04-13 15:17:17 +0200

    Create angular client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from "./login-page/login-page.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LoginPageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  user?: string;
+
+  protected displayLogin() {
+    return Boolean(this.user);
+  }
+}
