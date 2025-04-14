23e1692757118745697a9e7b7cc8eeb7a0548151
OscarMarcusson <oscar.marcusson@outlook.com>
2025-04-14 17:28:42 +0200

    Create basic login page

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { InputComponent } from "../input/input.component";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
-  selector: 'app-login-page',
+  selector: "app-login-page",
   standalone: true,
-  imports: [],
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  imports: [InputComponent, ButtonComponent],
+  templateUrl: "./login-page.component.html",
+  styleUrl: "./login-page.component.scss",
 })
 export class LoginPageComponent {
+  name: string = "";
+  password: string = "";
 
+  @ViewChild("nameField")
+  nameField!: InputComponent;
+
+  @ViewChild("passwordField")
+  passwordField!: InputComponent;
+
+  login() {
+    console.warn(this.nameField);
+    // event.preventDefault();
+    // event.stopPropagation();
+    console.log(this.name);
+    return false;
+  }
+
+  selectSubmit() {
+    throw new Error("Method not implemented.");
+  }
+  selectUserName() {
+    this.nameField.focus();
+  }
+  selectPassword() {
+    this.passwordField.focus();
+  }
 }
