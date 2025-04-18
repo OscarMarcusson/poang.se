38c8cf0d6331c2f5198d6be10344faee6282e3b2
OscarMarcusson <oscar.marcusson@outlook.com>
2025-04-18 20:35:45 +0200

    Add game overview with basic list implementation

--- client/src/app/game-list/game-list.component.ts
+++ client/src/app/game-list/game-list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [],
   templateUrl: './game-list.component.html',
-  styleUrl: './game-list.component.scss'
+  styleUrl: './game-list.component.scss',
+  imports: [ButtonComponent],
 })
 export class GameListComponent {
-  
+  games = input<string[]>([]);
+  title = input<string>("");
 }
--- client/src/app/overview/overview.component.ts
+++ client/src/app/overview/overview.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { GameListService } from '../services/game-list/game-list.service';
+import { GameListComponent } from "../game-list/game-list.component";
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [],
+  imports: [GameListComponent],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
 })
 export class OverviewComponent {
-  private games = inject(GameListService);
+  protected games = inject(GameListService);
 }
